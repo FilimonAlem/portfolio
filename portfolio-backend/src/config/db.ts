191463690b59dcb9f372ebc2f75ab14b53b8877c
Filimon Alem <filimonalem1@gmail.com>
2025-11-20 10:52:01 +0300

    Congig

--- portfolio-backend/src/config/db.ts
+++ portfolio-backend/src/config/db.ts
@@ -6,5 +6,5 @@ dotenv.config();
 const MONGO_URI = process.env.MONGO_URI!;
 const PORT = process.env.PORT || 5000;
 mongoose.connect(MONGO_URI)
-  .then(() => console.log('MongoDB Atlas connected 🚀'))
+  .then(() => console.log('MongoDB Atlas connected.'))
   .catch(err => console.error('MongoDB connection error:', err));
