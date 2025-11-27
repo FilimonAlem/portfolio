847d985a7cc08a51334aeb4029789fa296000668
Filimon Alem <filimonalem1@gmail.com>
2025-11-27 09:37:29 +0300

    config

--- portfolio-frontend/src/api/projects.ts
+++ portfolio-frontend/src/api/projects.ts
@@ -10,8 +10,9 @@ export interface Project {
 const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 export async function getProjects(): Promise<Project[]> {
-  const res = await fetch("https://portfolio-backend-hgi2.onrender.com/api/project");
+  const res = await fetch(`${VITE_API_URL}/api/projects`);
   if (!res.ok) throw new Error("Failed to fetch projects");
   const data: Project[] = await res.json();
+    console.log("Fetching projects from:", `${VITE_API_URL}/api/projects`);
   return data;
 } 
