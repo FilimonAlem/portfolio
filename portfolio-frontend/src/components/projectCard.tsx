6147d91ac612878cf7051352144210725fb4d5a7
Filimon Alem <filimonalem1@gmail.com>
2025-11-28 17:40:37 +0300

    config

--- portfolio-frontend/src/components/projectCard.tsx
+++ portfolio-frontend/src/components/projectCard.tsx
@@ -17,8 +17,7 @@ export default function ProjectCard({ project }: ProjectCardProps) {
   return (
     <div className="p-4 rounded-xl shadow-md bg-white">
       <h2 className="text-xl font-bold">{project.title}</h2>
-      <p>{project.description}</p>
-      <br />
+      <p className="mb-1">{project.description}</p>
       <p className="text-sm text-gray-600">{project.technologies.join(", ")}</p>
       <div className="mt-2 flex gap-3">
         <a
