672d2087b5aa3e8784eb36c6239018aa67a65855
Filimon Alem <filimonalem1@gmail.com>
2025-11-29 11:09:53 +0300

    config

--- portfolio-frontend/src/App.tsx
+++ portfolio-frontend/src/App.tsx
@@ -44,7 +44,7 @@ const HeaderSection = () => (
     className="mt-20 mb-5 px-6 py-10 bg-gray-300 flex justify-between items-center"
   >
     <div>
-      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-green-700 font-semibold">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold">
         Filimon Alem
       </h1>
       <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-green-700 font-semibold">
